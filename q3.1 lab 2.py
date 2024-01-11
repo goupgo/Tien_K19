3da660869f0c07d808aadab99276ab66ba1d0f32
AI1903 PFP191 To Duc Tien <89452304+goupgo@users.noreply.github.com>
2024-01-11 17:35:56 +0700

    Add files via upload

---
+++ q3.1 lab 2.py
@@ -0,0 +1,8 @@
+def recursive_sum(n):
+    if n == 0:
+        return 0
+    else:
+        return n + recursive_sum(n - 1)
+
+result = recursive_sum(10)
+print("Sum of numbers from 0 to 10:", result)
